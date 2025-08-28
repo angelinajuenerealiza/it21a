104e12ce5f804ee1cefc4d74cdbf3ce89d63c190
Josh Sandoy <leeken851@gmail.com>
2025-08-28 09:40:49 +0800

    Added subtraction, multiplication, and division

--- hello.js
+++ hello.js
@@ -4,6 +4,12 @@ num1 = 2;
 num2 = 3;
 
 sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
 
 console.log(sum);
+console.log(diff);
+console.log(prod);
+console.log(quo);
 
