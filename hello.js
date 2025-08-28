1bedabcf73b917da544e5607ce3f492b242c4313
Josh Sandoy <leeken851@gmail.com>
2025-08-28 09:31:57 +0800

    Initial Commit

--- hello.js
+++ hello.js
@@ -1,14 +1,1 @@
 console.log("Hello World");
-
-num1 = 2;
-num2 = 3;
-
-sum = num1 + num2;
-diff = num1 - num2;
-prod = num1 *num2;
-quo = num1 / num2;
-
-console.log(sum);
-console.log(diff);
-console.log(prod);
-console.log(quo);
