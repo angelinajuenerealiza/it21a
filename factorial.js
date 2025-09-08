a674a6fe5630c26974a9126759f3de0be01d33bb
angelinajuenerealiza <angeljuenereal@gmail.com>
2025-09-08 08:41:00 +0800

    added

--- factorial.js
+++ factorial.js
@@ -42,5 +42,20 @@ rl.question("Enter your choice (1-3)", choice =>{
 
 })
 }
-function computeFactorial(){}
+function computeFactorial(){ console.log("Factorial");
+        rl.question("Enter a number for factorial: ", (numStr) =>{
+            let num = parseInt(numStr);
+
+            if(isNaN(num) || num<0){
+                console.log("Please enter a non negative integer");
+            }else{
+                let fact  = 1;
+                for (let i=1; i<= num ; i++){
+                    fact *=i;
+                }
+                console.log("The factorial of " + num + " is " + fact);
+                }
+            });
+        }
+    
 mainMenu();
