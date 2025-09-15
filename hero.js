3ab090441134c887cd3a7fa4f56ee900c3af1786
angelinajuenerealiza <angeljuenereal@gmail.com>
2025-09-15 08:54:33 +0800

    added hero

---
+++ hero.js
@@ -0,0 +1,13 @@
+class Hero {
+    #health;
+
+    constructor (name,health,attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+    }
+
+    getHealth(){
+        return this.#health;
+    }
+}
