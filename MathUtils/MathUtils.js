b922f72b79d570f97c415f47a30cf2ff4998a4aa
angelinajuenerealiza <angeljuenereala@gmail.com>
2025-10-23 07:54:51 +0800

    addde radarchart.html

--- MathUtils/MathUtils.js
+++
@@ -1,8 +0,0 @@
-class MathUtils{
-    static computeFactorial(n){
-        if (n<0) throw new error("Number must be a non-negative");
-        let fact =1;
-        
-        for (let i = 1 i<=n ; +)
-    }
-}
